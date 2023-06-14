8fffcf8cc777c6f25fcb08de065dcd4592498706
Clayton Christian <claybowlproductions@gmail.com>
2023-06-13 23:42:42 -0500

    got it. thanks blake

---
+++ To seamlessly integrate Magicform with y.py
@@ -0,0 +1,98 @@
+To seamlessly integrate Magicform with your website, please refer to the instructions below for integrating the code with the appropriate location on your site.
+
+To begin, please follow these simple steps:
+
+1. Access your Magicform dashboard.
+2. On the left-hand side, click on the "Designer" button. If you already have a saved template,
+you can click on the pencil icon next to the one you wish to use. If you do not have a template,
+you can click on the "Starter Template" button to create a new one.
+3. Once you are in the desired template, locate the "Copy Embed Code" green button on the right-hand side and click on it.
+4. You are now ready to paste the code into the platform you wish to integrate Magicform with.
+
+Wix Instructions:
+
+- Instructions
+    
+    To add a line of code to your Wix store on your website, you can use the Wix Code editor. Here are the steps you can follow:
+    
+    1.  Log in to your Wix account and open the editor for the website you want to add the code 
+    2. Open your Wix website editor and look for a button on the left side that says "Add" (or the + symbol). Click on it.
+    3. From the options that appear, select "Embed Code".
+    4. There will be a section called "Popular Embeds". Click on it.
+    5. Look for a button that says "Embed HTML" and click on it.
+    6. A new window will open where you can paste a line of code. Paste the code there.
+    7. After pasting the code, click on "Apply" button to save the changes you made
+
+Wordpress Instructions
+
+3. On the right-hand side of the editor, you will see a list of theme files. These files control the different
+aspects of your website, such as the header, footer, and sidebar. To add the line of code to the appropriate place,
+you will need to select the file where you want to add the code. For example, if you want to add
+the code to the header of your website, you would select the "header.php" file.
+4. Once you have selected the appropriate file, you will see the code for that file displayed in the editor. You can
+    scroll through the code to find the spot where you want to add the new line of code. It's generally
+recommended to add the code just before the closing body tag.
+5. Once you have found the spot where you want to add the code, you can simply type or paste it in. 
+6. After adding the code, make sure to save your changes by clicking the "Update File" button at the bottom of the editor.
+7. It's recommended to test your website after adding the code to make sure it's working as expected and
+to ensure that the code didn't break any functionality of your website.
+
+Note: It's always recommended to have a backup of your website before making any modifications or adding
+any code. This way if anything goes wrong, you can easily restore your website to the previous working state.
+
+Webflow Deployment instructions:
+
+2. Once you are in the Designer, you will see the layout of your website on the left-hand side and the
+settings on the right-hand side. To add code to a specific page, you will need to navigate to that page by
+clicking on the "Pages" button in the top navigation bar, then select the page you want to add the code to.
+3. Once you are on the specific page you want to add the code to, you will see the layout of the page in the
+Designer. You can select the element on the page that you want to add the code to, just like you would if you were adding code to the entire site.
+4. After selecting the element, you will see the settings for that element on the right-hand side. Scroll down to the
+    "Custom Code" section.
+5. In the "Custom Code" section, you will see options for "Before </head>" and "Before </body>" . You can use
+    these options to add code to the head or body section of your specific page. If you want to add the code in a specific
+area of the element, you can use the "Custom CSS" or "Custom JavaScript" sections.
+6. Once you have found the appropriate section, you can type or paste the line of code in the designated text area.
+7. After adding the code, make sure to save your changes by clicking the "Save" button at the top of the Designer.
+8. IOnce you are on the specific page you want to add the code to, you will see the layout of the page in the Designer.
+You can select the element on the page that you want to add the code to, just like you would if you were adding code to the entire site.
+4. After selecting the element, you will see the settings for that element on the right-hand side. Scroll down to the "Custom Code" section.
+5. In the "Custom Code" section, you will see options for "Before </head>" and "Before </body>" . You can
+    use these options to add code to the head or body section of your specific page. If you want
+to add the code in a specific area of the element, you can use the "Custom CSS" or "Custom JavaScript" sections.
+6. Once you have found the appropriate section, you can type or paste the line of code in the designated text area.
+7. After adding the code, make sure to save your changes by clicking the "Save" button at the top of the Designer.
+8. It's recommended to test your website after adding the code to make sure it's working as expected
+and to ensure that the code didn't break any functionality of your website.
+
+Note: It's always recommended to have a backup of your website before making any modifications or
+adding any code. This way if anything goes wrong, you can easily restore your website to the previous working state.
+
+Hubspot Instructions:
+
+8. To check your modification, you can preview your website by clicking the "Preview" button at the top
+of the code editor or by navigating to your website from the front end.
+
+SquareSpace Instructions
+
+It's important to note that making modifications to your code could affect other parts of your website, so it's
+important to make a backup of your website before making any modifications or adding any code. Also, it's
+important to test your website after adding the code to make sure it's working as expected and to ensure that the code didn't break any functionality of your website.
+
+Square Online Instruction:
+
+To add a line of code to your Square website, you will need to use the website building platform provided by Square, Square Online.
+
+1. Log in to your Square account and go to the Square Dashboard.
+2. On the left sidebar, click on "Online" and then "Website" then “Edit Site”.
+3. From your Square Online site editor, open the page you want to add our embed code to 
+4. Select the “+Add” button on the top left side and choose Embed code section
+5. In the editing panel, select Embed Code and paste your code into the text box on the left
+6. Once you have added your code, preview it by clicking on the "Preview" button. This will allow you to see how your changes will look on your live website.
+7. Once you are satisfied with your changes, click on the "Save" button to save your changes.
+
+It is important to note that modifying your website's code, particularly if you are not familiar with the
+code, can cause issues with your website. Be sure to test the changes thoroughly and make a backup of your website before making any code modifications.t's recommended to test your website after adding the code to make sure it's working as expected and to ensure that the code didn't break any functionality of your website.
+
+Note: It's always recommended to have a backup of your website before making any modifications or adding
+any code. This way if anything goes wrong, you can easily restore your website to the previous working state.
